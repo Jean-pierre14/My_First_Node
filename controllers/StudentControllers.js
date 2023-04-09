41819908e0c94a54cecc52ae20782849f0135f90
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-04-09 21:09:21 +0200

    Deleteone and deletemany... The APIs student is ready

--- controllers/StudentControllers.js
+++ controllers/StudentControllers.js
@@ -1,7 +1,4 @@
 import Student from "./../models/Student.js";
-import bcrypt from "bcrypt";
-
-import { body, validationResult } from "express-validator";
 
 export const GetStudents = async (req, res) => {
   try {
@@ -19,9 +16,15 @@ export const GetStudent = async (req, res) => {
     .catch((error) => res.status(500).json({ msg: error.message }));
 };
 
-export const DeleteStudent = (req, res) => {
-  res.json("Student Delete one");
+export const DeleteStudent = async (req, res) => {
+  let studentId = req.params.id;
+  await Student.findOneAndDelete(studentId)
+    .then((student) => res.json(student))
+    .catch((err) => console.log(err));
 };
-export const DeleteAllStudents = (req, res) => {
-  res.json("Student Delete All");
+
+export const DeleteAllStudents = async (req, res) => {
+  await Student.deleteMany({})
+    .then((_) => res.json({ msg: "Collection is clean" }))
+    .catch((err) => console.log(err));
 };
