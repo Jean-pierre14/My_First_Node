fd35b69f74572f3cd77bdfd48371743aa38df42d
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-04-13 15:43:05 +0200

    User APIs

---
+++ controllers/UserControllers.js
@@ -0,0 +1,20 @@
+import User from "../models/userModel.js";
+
+export const GetUsers = async (req, res) => {
+  await res.json("Get all Users");
+};
+export const GetUser = async (req, res) => {
+  await res.json("Get User");
+};
+export const PostUser = async (req, res) => {
+  await res.json("Post all Users");
+};
+export const UpdateUsers = async (req, res) => {
+  await res.json("Get all Users");
+};
+export const DeleteUsers = async (req, res) => {
+  await res.json("Delete all Users");
+};
+export const DeleteUser = async (req, res) => {
+  await res.json("Get all Users");
+};
