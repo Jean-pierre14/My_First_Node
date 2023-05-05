53485c583f54e2f9e0ee413add052a6cef1f2a1b
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-05-05 12:40:27 +0300

    Journal model fix

--- models/journal.js
+++ models/journal.js
@@ -1,13 +1,8 @@
 import { Schema, model } from "mongoose";
 
 const journal = new Schema({
-  className: { type: String, required: true },
-  titulaire: { type: String, required: true },
-  semaine: { type: String, required: true },
-  faitA: { type: String, required: true },
-  SchoolName: { type: String, required: true },
-  AnneeScolaire: { type: String, required: true },
-  Logo: { type: String, required: true },
+  cours: { type: String, required: true },
+  lecon: { type: String, required: true },
 });
 
 const Journal = model("journal", journal);
