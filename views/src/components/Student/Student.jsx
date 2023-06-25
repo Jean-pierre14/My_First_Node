6a100b559a880d044bf224bdcd3fa9d2367f71af
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-06-25 15:50:29 +0200

    add: students

--- views/src/components/Student/Student.jsx
+++ views/src/components/Student/Student.jsx
@@ -15,17 +15,17 @@ const Student = () => {
           <input type="search" name="search" id="search" placeholder="Search..." className="form-control" />
       </form>
       <ul className="list-group">
-      {donnee.map((item) => (
-        <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-          <span>{item.fullname }</span>
-          <div>
+        {donnee.map((item) => {
+          <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+            <span>{item.fullname}</span>
+            <div>
               <div className="btn-group">
-                  <a href="#" className="btn btn-sm btn-info">Edit</a>
-                  <a href="#" className="btn btn-sm btn-danger">Delete</a>
+                <a href="#" className="btn btn-sm btn-info">Edit</a>
+                <a href="#" className="btn btn-sm btn-danger">Delete</a>
               </div>
-          </div>
-        </li>
-      ))}
+            </div>
+          </li>
+        })}
       </ul>
     </>
   )
